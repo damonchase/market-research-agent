a341b3ba16574d29eb476e31da2d0c6b32b47e6d
Damon Chase <chasedamon447@gmail.com>
2026-01-01 15:21:23 -0500

    Create new generate response endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
+import ReactMarkdown from 'react-markdown'
 import { useEffect, useState } from "react";
 
 function App() {
   const [msg, setMsg] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/hello")
+    fetch("http://localhost:8000/api/generate_response", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"prompt": "Why is the sky blue"})
+    })
       .then(res => res.json())
-      .then(data => setMsg(data.message));
+      .then(data => setMsg(data.text));
   }, []);
 
-  return <h1>{msg}</h1>;
+  return <ReactMarkdown>{msg}</ReactMarkdown>;
 }
 
 export default App;
--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+
+_ = load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
 
 app = FastAPI()
 
@@ -10,7 +20,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class PromptRequest(BaseModel):
+    prompt: str
 
 @app.get("/api/hello")
 def hello():
     return {"message": "Hello from Python!"}
+
+@app.post("/api/generate_response")
+def generate_response(request: PromptRequest):
+    content = types.Content(
+            role="user",
+            parts=[types.Part(text=request.prompt)]
+        )
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=content
+    )
+
+    return {"text": response.text}
