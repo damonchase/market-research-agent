e28423dc8bdf286ee0a0c7e90b45d48172133bb0
Damon Chase <chasedamon447@gmail.com>
2026-01-04 14:53:55 -0500

    Add button group for selecting graph

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import ReactMarkdown from 'react-markdown'
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import { useState } from "react";
+import '@fontsource/roboto/700.css';
 
 function App() {
   const [request, setRequest] = useState("");
   const [response, setResponse] = useState("");
   const [sent, setSent] = useState(false)
+  const [graph, setGraph] = useState("1D")
   
   const handleSend = async () => {
     const res = await fetch("http://localhost:8000/api/generate_response", {
@@ -41,8 +44,17 @@ function App() {
         <Box sx={{ mt: 2, width: '100%', maxWidth: '600px' }}>
           <ReactMarkdown>{response}</ReactMarkdown>
         </Box>
+        <Box sx={{ mt: 1, mb: 1 }}>
+          {sent && <ButtonGroup variant="contained" aria-label="Basic button group">
+            <Button onClick={() => setGraph("1D")}>1D</Button>
+            <Button onClick={() => setGraph("1W")}>1W</Button>
+            <Button onClick={() => setGraph("1M")}>1M</Button>
+          </ButtonGroup>}
+        </Box>
         <Box>
-          {sent && <img src="/soccer-ball.jpg" alt="Test" />}
+          {sent && graph==="1D" && <img src="/1D-Graph.jpg" alt="Graph of stock data for the past day" />}
+          {sent && graph==="5D" && <img src="/5D-Graph.jpg" alt="Graph of stock data for the past 5 days" />}
+          {sent && graph==="1M" && <img src="/1M-Graph.jpg" alt="Graph of stock data for the past month" />}
         </Box>
       </Box>
     </div>
