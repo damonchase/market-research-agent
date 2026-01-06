969c6337e53567e98617d0ce67f35768e50790a4
Damon Chase <chasedamon447@gmail.com>
2026-01-06 16:54:51 -0500

    Update navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Navbar.css";
 
 function Navbar() {
+    const location = useLocation();
+
     return (
-        <nav>
-            <Link to="/">Research</Link>
-            <Link to="/articles">Articles</Link>
+        <nav className="navbar">
+            <div className="navbar-brand">StockSense</div>
+            <div className="navbar-links">
+                <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+                    Research
+                </Link>
+                <Link to="/articles" className={location.pathname === "/articles" ? "active" : ""}>
+                    Articles
+                </Link>
+            </div>
+            <div className="navbar-spacer"></div> 
         </nav>
     );
 }
 
-export default Navbar
+export default Navbar;
