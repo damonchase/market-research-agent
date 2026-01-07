c3c55c1fb9bfc2d78bb56e616b796d184ddcdbdd
Damon Chase <chasedamon447@gmail.com>
2026-01-07 15:21:46 -0500

    Implement the articles page with placeholders

--- client/src/pages/Articles.js
+++ client/src/pages/Articles.js
@@ -1,9 +1,26 @@
-import '@fontsource/roboto/700.css';
+import React from 'react';
+import "./../styles/Articles.css"
 
 function Articles() {
+  const articles = [
+    { id: 1, title: "Article 1 Place Holder", url: "/" },
+    { id: 2, title: "Article 2 Place Holder", url: "/"}
+  ];
+
   return (
-        <h1>Articles!</h1>
-    );
+    <div className="main-container" style={{ padding: '20px', maxWidth: "600px", margin: '0 auto' }}>
+      <h1>Articles</h1>
+      <div className="article-list">
+        {articles.map((article) => (
+          <div key={article.id} className="article-row">
+            <a href={article.url} className="article-link">
+              {article.title}
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Articles;
